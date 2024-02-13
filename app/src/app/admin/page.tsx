9b3e75bf11bf7e3d6aea08a41364b257da823cd0
土屋隆征 <pc.tsuchiya@gmail.com>
2024-02-13 23:37:20 +0900

    mui init

---
+++ app/src/app/admin/page.tsx
@@ -0,0 +1,25 @@
+import { Button, Grid, Stack } from '@mui/material';
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+<Grid
+      container
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      direction="column"
+    >
+      <h1>Using MUI with Next.js 13</h1>
+      <Stack direction="row" columnGap={1}>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </Grid>
+    </>
+  )
+}
+
+export default Page;
---
+++ app/src/app/detail/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DetailLayout = ( { children }: { children: React.ReactNode } ) => {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default DetailLayout
