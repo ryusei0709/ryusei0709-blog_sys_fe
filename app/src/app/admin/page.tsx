ce5a74c5a3071a5c08eb8a26ba1f82eb35bf46f4
土屋隆征 <pc.tsuchiya@gmail.com>
2024-02-15 10:10:27 +0900

    mui init

--- app/src/app/admin/page.tsx
+++ app/src/app/admin/page.tsx
@@ -4,20 +4,20 @@ import React from 'react'
 const Page = () => {
   return (
     <>
-<Grid
-      container
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      direction="column"
-    >
-      <h1>Using MUI with Next.js 13</h1>
-      <Stack direction="row" columnGap={1}>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-    </Grid>
+      <Grid
+        container
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        >
+          <h1>Using MUI with Next.js 13</h1>
+          <Stack direction="row" columnGap={1}>
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+          </Stack>
+      </Grid>
     </>
   )
 }
